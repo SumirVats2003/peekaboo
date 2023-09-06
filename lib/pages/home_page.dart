1a38e447212183f62586ec7b13572631c740accd
Sumir Vats <sumirvats@gmail.com>
2023-09-06 19:43:30 +0530

    Added Chats Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:peekaboo/services/auth/auth_service.dart';
 import 'package:provider/provider.dart';
+
+import 'chat_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +14,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // instance of auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   // sign user out
   void signOut() {
     final authService = Provider.of<AuthService>(context, listen: false);
@@ -28,6 +35,51 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      body: _buildUserList(),
 		);
+  }
+
+  // build a list of users excpet for the current user
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading..");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs
+          .map<Widget>((doc) => _buildUserListItem(doc))
+          .toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildUserListItem(DocumentSnapshot document){
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+    if (_auth.currentUser!.email != data['email']){
+      return ListTile(
+        title: Text(data['email']),
+        onTap: () {
+          Navigator.push(
+            context, 
+            MaterialPageRoute(builder: (context)=>ChatPage(
+              receiverUserEmail: data['email'],
+              receiverUserId: data['uid'],
+            ))
+          );
+        },
+      );
+    }
+
+    else {
+      return Container();
+    }
   }
 }
